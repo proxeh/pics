dd265331b2830e665b80c1e2dda06c443db52a6d
Jamie Shaw <proxeh@gmail.com>
2023-06-21 10:57:17 +0100

    Images App
    Basic app functionality

---
+++ src/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const searchImages = async (term) => {
+	const response = await axios.get("https://api.unsplash.com/search/photos", {
+		headers: {
+			Authorization:
+				"Client-ID c6bc3a781cc03bac96478b10c28b7d41976c3bc1f66a5abc3783d95e59b257d0",
+		},
+		params: {
+			query: term,
+		},
+	});
+
+	console.log(response.data.results);
+
+	return response.data.results;
+};
+
+export default searchImages;
